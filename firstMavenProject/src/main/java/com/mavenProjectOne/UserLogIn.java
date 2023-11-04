5d7ac4bad826dbe4c080d33d6b799ad75af03528
nabin <nkbhaie2017@gmail.com>
2023-11-03 21:52:14 -0600

    add missing folders for test

--- firstMavenProject/src/main/java/com/mavenProjectOne/UserLogIn.java
+++ firstMavenProject/src/main/java/com/mavenProjectOne/UserLogIn.java
@@ -7,7 +7,7 @@ public class UserLogIn {
 		ResourceBundle rb = ResourceBundle.getBundle("config");
 		String username = rb.getString("username");
 		String password = rb.getString("password");
-		
+
 		if(in_user.equals(username) && in_password.equals("password")) {
 			return 1;
 		}else {
